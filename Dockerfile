73bde85f1b4500cb3134e7c374659e120840bd65
Aslan <asl.zurabov@gmail.com>
2022-01-31 13:05:42 +0300

    AWS S3

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+# Python and Linux Version
+FROM python:3.10.0a1-alpine3.12
--- core/settings/storage.py
+++ core/settings/storage.py
@@ -1,4 +1,34 @@
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+import os
+import environ
+from pathlib import Path
 
-STATIC_URL = 'static/'
+from core.settings.settings import BASE_DIR 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+env = environ.Env()
+environ.Env.read_env()
+
+# Boto3
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = None
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.us-east-1.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# A path prefix that will be prepended to all uploads
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{S3_URL}/{AWS_LOCATION}/'
+
+
+
+
+# Django Static Files Directory
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+
+
